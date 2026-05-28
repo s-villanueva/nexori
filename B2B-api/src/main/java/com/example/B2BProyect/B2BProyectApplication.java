72ed563fabc35df5b6dfc8e9bdab16556037df4a
llas <nicolascresposuarez@gmail.com>
2026-05-28 18:28:48 -0400

    Today's class changes made.

--- B2B-api/src/main/java/com/example/B2BProyect/B2BProyectApplication.java
+++ B2B-api/src/main/java/com/example/B2BProyect/B2BProyectApplication.java
@@ -1,31 +1,27 @@
 package com.example.B2BProyect;
 
+import com.example.B2BProyect.service.integration.SistemaA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.data.web.config.SpringDataWebSettings;
+import org.springframework.core.ParameterizedTypeReference;
 
 @SpringBootApplication
 public class B2BProyectApplication implements CommandLineRunner {
 	@Autowired
-    private SpringDataWebSettings springDataWebSettings;
+	private SistemaA sistemaA;
 
 	public static void main(String[] args) {
 		SpringApplication.run(B2BProyectApplication.class, args);
-
-		System.out.println("Hola mundo");
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		utilitario();
-	}
-
-	private void utilitario() {
-//		Prueba prueba = new Prueba();
-//		prueba.setNombre("Nueva prueba uwu");
-//		pruebaRepository.save(prueba);
+		sistemaA.getAll(
+				"/api/v1/sucursales-empresa",
+				new ParameterizedTypeReference<>() {}
+		);
 	}
 
 
